177ff38edab8e0c6b5f09d346752c62459803134
iumag <iumag@yandex.ru>
2018-12-21 23:52:06 +0800

    * rever Program.cs

--- FS_TI MicroService/Program.cs
+++ FS_TI MicroService/Program.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
-using System.Threading;
 
 namespace FS_TI_MicroService
 {
@@ -18,31 +17,12 @@ namespace FS_TI_MicroService
         /// </summary>
         static void Main()
         {
-            // init cur dir
-            Program.curDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
-            Settings.cfgFile = Program.curDirectory + "\\config.json";
-            // read config
-            Program.cfg = Settings.Read();
-
-            // try to run web server
-            if (!runWebServer())
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
             {
-               
-            }
-        }
-
-        static bool runWebServer()
-        {
-            int port = Program.cfg.security.port;
-            if (!(port > 0))
-            {
-          
-                return false;
-            }
-            Program.srv = new MyHttpServer(17301);
-            Thread thread = new Thread(new ThreadStart(Program.srv.listen));
-            thread.Start();
-            return true;
+                new ServiceMain()
+            };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
